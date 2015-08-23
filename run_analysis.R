8e705f99733176bd7a80461fc258c97c01753759
Alyse Lin <Alyselin@users.noreply.github.com>
2015-08-23 11:01:29 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,15 @@
+#Alyse, Aug23, 2015, R version 3.1.2 (2014-10-31),x86_64, mingw32
+
+#clean workspace and setup working directory
 remove(list=ls())
 setwd("C:\\Users\\linw24\\Desktop\\WK3")
 getwd()
 
+#create a folder and download files
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
-
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
@@ -20,33 +23,32 @@ dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),h
 #Read Fearures files
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
-
+#check varibles
 str(dataActivityTest)
 str(dataActivityTrain)
 str(dataSubjectTrain)
 str(dataSubjectTest)
 str(dataFeaturesTest)
 str(dataFeaturesTrain)
-
+#merges the training and the test sets
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
 dataActivity<- rbind(dataActivityTrain, dataActivityTest)
 dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
-
+#naming
 names(dataSubject)<-c("subject")
 names(dataActivity)<- c("activity")
 dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
 names(dataFeatures)<- dataFeaturesNames$V2
-
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
-
+#subset naming
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
 selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
 str(Data)
 activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
 head(Data$activity,30)
-
+#labeling
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
 names(Data)<-gsub("Acc", "Accelerometer", names(Data))
@@ -54,7 +56,7 @@ names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
 names(Data)
-
+#call libries and create table and codebook
 version
 library(plyr)
 Data2<-aggregate(. ~subject + activity, Data, mean)
